2456e38da408b5225964835c24c72d57a8b2dbc2
Vikash Kumar <kvikash766@gmail.com>
2025-01-02 17:51:09 +0100

    powered by chatwoot removed

--- app/javascript/shared/components/Branding.vue
+++ app/javascript/shared/components/Branding.vue
@@ -24,23 +24,6 @@ export default {
       },
     };
   },
-  computed: {
-    brandRedirectURL() {
-      try {
-        const referrerHost = this.$store.getters['appConfig/getReferrerHost'];
-        const baseURL = `${this.globalConfig.widgetBrandURL}?utm_source=${
-          referrerHost ? 'widget_branding' : 'survey_branding'
-        }`;
-        if (referrerHost) {
-          return `${baseURL}&utm_referrer=${referrerHost}`;
-        }
-        return baseURL;
-      } catch (e) {
-        // Suppressing the error as getter is not defined in some cases
-      }
-      return '';
-    },
-  },
 };
 </script>
 
@@ -49,21 +32,9 @@ export default {
     v-if="globalConfig.brandName && !disableBranding"
     class="px-0 py-3 flex justify-center"
   >
-    <a
-      :href="brandRedirectURL"
-      rel="noreferrer noopener nofollow"
-      target="_blank"
-      class="branding--link justify-center items-center leading-3"
-    >
-      <img
-        class="branding--image"
-        :alt="globalConfig.brandName"
-        :src="globalConfig.logoThumbnail"
-      />
-      <span>
-        {{ useInstallationName($t('POWERED_BY'), globalConfig.brandName) }}
-      </span>
-    </a>
+    <span class="branding--text justify-center items-center leading-3">
+      {{ useInstallationName($t('POWERED_BY'), globalConfig.brandName) }}
+    </span>
   </div>
   <div v-else class="p-3" />
 </template>
@@ -71,25 +42,10 @@ export default {
 <style scoped lang="scss">
 @import 'widget/assets/scss/variables.scss';
 
-.branding--image {
-  margin-right: $space-smaller;
-  max-width: $space-slab;
-  max-height: $space-slab;
-}
-
-.branding--link {
+.branding--text {
   color: $color-light-gray;
-  cursor: pointer;
-  display: inline-flex;
-  filter: grayscale(1);
   font-size: $font-size-small;
   opacity: 0.9;
-  text-decoration: none;
-
-  &:hover {
-    filter: grayscale(0);
-    opacity: 1;
-    color: $color-gray;
-  }
+  text-align: center;
 }
 </style>
